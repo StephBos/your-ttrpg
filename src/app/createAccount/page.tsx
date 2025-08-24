3b1c952e03ca657a4894a3ed8f10cf4747488fa1
Stephen Bos <stephen.bos1029@gmail.com>
2025-08-24 12:43:28 -0600

    added image transitions

--- src/app/createAccount/page.tsx
+++ src/app/createAccount/page.tsx
@@ -42,15 +42,17 @@ export default function Home() {
         ))}
       </div>
       
-      <main className="flex flex-col row-start-2 items-center sm:items-start bg-gray-900/90 p-6 rounded gap-2 relative z-10">
-        
+      <main className="flex flex-col row-start-2 items-center sm:items-start bg-gray-900/90 p-6 rounded gap-2 relative z-10">      
         <form action="submit" className="flex flex-col gap-2">
-            <h1 className="text-3xl font-bold">Login</h1>
+            <h1 className="text-3xl font-bold">Create Account</h1>
+            <input type="text" name="username" className="bg-gray-800 rounded p-1 w-64" placeholder="Email"/>
+            <input type="text" name="username" className="bg-gray-800 rounded p-1 w-64" placeholder="Confirm Email"/>
             <input type="text" name="username" className="bg-gray-800 rounded p-1 w-64" placeholder="Username"/>
             <input type="text" name="password" className="bg-gray-800 rounded p-1 w-64" placeholder="Password"/>
+            <input type="text" name="password" className="bg-gray-800 rounded p-1 w-64" placeholder="Confirm Password"/>
             <button className="bg-indigo-950 hover:bg-indigo-900 w-64 rounded">Login</button>
             <a className="text-sm text-gray-400 hover:text-gray-300" href="/forgotPassword">Forgot your password?</a>
-            <a className="text-sm text-gray-400 hover:text-gray-300" href="/createAccount">Create Account</a>
+            <a className="text-sm text-gray-400 hover:text-gray-300" href="/">Already have an Account? Login</a>
         </form>
       </main>
     </div>
