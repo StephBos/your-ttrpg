fe21826fc66666661080591783a7bd224af70489
Stephen Bos <stephen.bos1029@gmail.com>
2025-09-01 22:22:16 -0600

    switching to checking one username at a time

--- src/app/createAccount/page.tsx
+++ src/app/createAccount/page.tsx
@@ -5,34 +5,45 @@ import CreateAccountHelper from './createAccountHelper'
 export default function Home() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [isTransitioning, setIsTransitioning] = useState(false);
-  const [userNames, setUsernames] = useState([])
+  const [userNameExists, setUsernameExists] = useState(false)
+  const [buttonDisabled, setButtonDisabled] = useState(false)
   
   const backgroundImages = [
     '/background1.jpg',
     '/background2.jpg', 
     '/background3.jpg'
-  ];
-
-  useEffect(() => {
-    CreateAccountHelper.fetchUsers().then(allUsernames => {
-      setUsernames(allUsernames)
-    })
-  }, [])
+  ]
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIsTransitioning(true);
+      setIsTransitioning(true)
       
       setTimeout(() => {
         setCurrentImageIndex((prevIndex) => 
           (prevIndex + 1) % backgroundImages.length
         )
-        setIsTransitioning(false);
+        setIsTransitioning(false)
       }, 1000)//Transition duration
     }, 5000)//Change image every 5 seconds
     
     return () => clearInterval(interval)
   }, [backgroundImages.length])
+
+  async function checkIfUsernameExists(username: string): Promise<boolean> {
+    console.log('getting all users')
+    try {
+      const response = await fetch("http://localhost:3000/users?username=" + username)
+            
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      
+      return true
+    } catch (error) {
+      console.error('Error fetching users:', error)
+      return false
+    }
+  }
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 relative overflow-hidden">
@@ -55,10 +66,10 @@ export default function Home() {
             <h1 className="text-3xl font-bold">Create Account</h1>
             <input type="text" name="email" className="bg-gray-800 rounded p-1 w-64" placeholder="Email"/>
             <input type="text" name="confirmEmail" className="bg-gray-800 rounded p-1 w-64" placeholder="Confirm Email"/>
-            <input type="text" name="username" className="bg-gray-800 rounded p-1 w-64" placeholder="Username"/>
+            <input type="text" name="username" className="bg-gray-800 rounded p-1 w-64" placeholder="Username" onChange={e => checkIfUsernameExists(e.target.value).then((exists: boolean) => setUsernameExists(exists))}/>
             <input type="text" name="password" className="bg-gray-800 rounded p-1 w-64" placeholder="Password"/>
             <input type="text" name="confirmPassword" className="bg-gray-800 rounded p-1 w-64" placeholder="Confirm Password"/>
-            <button className="bg-indigo-950 hover:bg-indigo-900 w-64 rounded">Create Account</button>
+            <button className="bg-indigo-950 hover:bg-indigo-900 w-64 rounded" disabled={buttonDisabled}>Create Account</button>
             <a className="text-sm text-gray-400 hover:text-gray-300" href="/forgotPassword">Forgot your password?</a>
             <a className="text-sm text-gray-400 hover:text-gray-300" href="/">Already have an Account? Login</a>
         </form>
