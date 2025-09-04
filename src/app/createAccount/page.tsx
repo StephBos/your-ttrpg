a3a31d2bdb87094ded174850163ad71839da71c9
Stephen Bos <stephen.bos1029@gmail.com>
2025-09-04 10:23:13 -0600

    wip

--- src/app/createAccount/page.tsx
+++ src/app/createAccount/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React, { useState, useEffect} from "react"
-import CreateAccountHelper from './createAccountHelper'
 
 export default function Home() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
