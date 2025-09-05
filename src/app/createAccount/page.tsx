bb22d625fc4060764ced47cdda0920c3f5ffa567
Stephen Bos <stephen.bos1029@gmail.com>
2025-09-05 17:12:39 -0600

    working on create account validation

--- src/app/createAccount/page.tsx
+++ src/app/createAccount/page.tsx
@@ -1,17 +1,38 @@
 "use client"
 import React, { useState, useEffect} from "react"
+import validator from "validator"
+import PasswordValidator from "password-validator"
 
 export default function Home() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [isTransitioning, setIsTransitioning] = useState(false);
-  const [userNameExists, setUsernameExists] = useState(false)
-  const [buttonDisabled, setButtonDisabled] = useState(false)
+  const [buttonDisabled, setButtonDisabled] = useState(true)
+  const [emailGood, setEmailGood] = useState({valid: false, error: ""})
+  const [confirmEmailGood, setConfirmEmailGood] = useState({valid: false, error: ""})
+  const [usernameGood, setUsernameGood] = useState({valid: false, error: ""})
+  const [passwordGood, setPasswordGood] = useState({valid: false, error: ""})
+  const [confirmPasswordGood, setConfirmPasswordGood] = useState({valid: false, error: ""})
+  const [email, setEmail] = useState("")
+  const [confirmEmail, setConfirmEmail] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
   
+
   const backgroundImages = [
     '/background1.jpg',
     '/background2.jpg', 
     '/background3.jpg'
   ]
+
+  useEffect(() => {
+    if (emailGood.valid && confirmEmailGood.valid && usernameGood.valid && passwordGood.valid && confirmPasswordGood.valid) {
+      setButtonDisabled(false)
+    } else {
+      setButtonDisabled(true)
+    }
+  }, [emailGood, confirmEmailGood, usernameGood, passwordGood, confirmPasswordGood])
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -28,11 +49,11 @@ export default function Home() {
     return () => clearInterval(interval)
   }, [backgroundImages.length])
 
-  async function checkIfUsernameExists(username: string): Promise<boolean> {
-    console.log('getting all users')
+  async function validateUsername(username: string): Promise<boolean> {
+    console.log('checking if username exists')
     try {
-      const response = await fetch("http://localhost:3000/users?username=" + username)
-            
+      const response = await fetch("http://localhost:3000/users/" + username)
+      console.log(response)
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`)
       }
@@ -42,6 +63,35 @@ export default function Home() {
       console.error('Error fetching users:', error)
       return false
     }
+  }
+
+  async function validateEmail(email: string, isConfirmEmail: boolean) {
+    console.log('Validating email')
+    if (validator.isEmail(email)) {
+      if (isConfirmEmail) {
+        if (email === confirmEmail) {
+          setConfirmEmailGood({valid: true, error: ""})
+        } else {
+          setConfirmEmailGood({valid: false, error: "Emails do not match"})
+        }
+      } else  {
+        setEmailGood({valid: true, error: ""})
+      }
+    } else {
+      setEmailGood({valid: false, error: "Invalid email"})
+    }
+  }
+
+  async function validatePassword(password: string, isConfirmPassword: boolean): Promise<boolean> {
+    console.log('Validating password')
+    return false
+  }
+
+
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    console.log('Creating account')
   }
 
   return (
@@ -63,12 +113,27 @@ export default function Home() {
       <main className="flex flex-col row-start-2 items-center sm:items-start bg-gray-900/90 p-6 rounded gap-2 relative z-10">      
         <form action="submit" className="flex flex-col gap-2">
             <h1 className="text-3xl font-bold">Create Account</h1>
-            <input type="text" name="email" className="bg-gray-800 rounded p-1 w-64" placeholder="Email"/>
-            <input type="text" name="confirmEmail" className="bg-gray-800 rounded p-1 w-64" placeholder="Confirm Email"/>
-            <input type="text" name="username" className="bg-gray-800 rounded p-1 w-64" placeholder="Username" onChange={e => checkIfUsernameExists(e.target.value).then((exists: boolean) => setUsernameExists(exists))}/>
-            <input type="text" name="password" className="bg-gray-800 rounded p-1 w-64" placeholder="Password"/>
-            <input type="text" name="confirmPassword" className="bg-gray-800 rounded p-1 w-64" placeholder="Confirm Password"/>
-            <button className="bg-indigo-950 hover:bg-indigo-900 w-64 rounded" disabled={buttonDisabled}>Create Account</button>
+            <input type="text" name="email" className="bg-gray-800 rounded p-1 w-64" placeholder="Email" onChange={(e) => {
+              validateEmail(e.target.value, false)
+              setEmail(e.target.value)
+            }}/>
+            <input type="text" name="confirmEmail" className="bg-gray-800 rounded p-1 w-64" placeholder="Confirm Email" onChange={(e) => {
+              validateEmail(e.target.value, true)
+              setConfirmEmail(e.target.value)
+            }}/>
+            <input type="text" name="username" className="bg-gray-800 rounded p-1 w-64" placeholder="Username" onChange={(e) => {
+              validateUsername(e.target.value)
+              setUsername(e.target.value)
+            }}/>
+            <input type="text" name="password" className="bg-gray-800 rounded p-1 w-64" placeholder="Password" onChange={(e) => {
+              validatePassword(e.target.value, false)
+              setPassword(e.target.value)
+            }}/>
+            <input type="text" name="confirmPassword" className="bg-gray-800 rounded p-1 w-64" placeholder="Confirm Password" onChange={(e) => {
+              validatePassword(e.target.value, true)
+              setConfirmPassword(e.target.value)
+             }} />
+            <button className="bg-indigo-950 hover:bg-indigo-900 w-64 rounded disabled:bg-gray-400 disabled:text-gray-700 disabled:cursor-not-allowed" disabled={buttonDisabled} onClick={handleSubmit}>Create Account</button>
             <a className="text-sm text-gray-400 hover:text-gray-300" href="/forgotPassword">Forgot your password?</a>
             <a className="text-sm text-gray-400 hover:text-gray-300" href="/">Already have an Account? Login</a>
         </form>
