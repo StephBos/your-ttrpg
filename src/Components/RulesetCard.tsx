1e705f9cde4d07fbf5785229b4046aaadde4d297
Stephen Bos <stephen.bos1029@gmail.com>
2025-11-17 23:27:40 -0700

    More card changes

--- src/Components/RulesetCard.tsx
+++ src/Components/RulesetCard.tsx
@@ -1,4 +1,5 @@
 import { Metal_Mania } from "next/font/google"
+import { Pirata_One } from "next/font/google"
 
 interface RulesetCardProps {
    title: string
@@ -8,6 +9,7 @@ interface RulesetCardProps {
 }
 
 const metalMania = Metal_Mania({ subsets: ['latin'], weight: '400' })
+const pirataOne = Pirata_One({ subsets: ['latin'], weight: '400' })
 
 export default function RulesetCard({
    title,
@@ -16,15 +18,15 @@ export default function RulesetCard({
    backgroundImageUrl,
 }: RulesetCardProps) {
    return (
-      <button className="flex flex-col items-start border-4 border-[#5C4033] p-4 rounded-lg bg-[#395144] shadow-lg z-10 w-60 h-80 gap-2 cursor-pointer hover:bg-[#4E6C50]/80">
-         <h1 className={`text-2xl text-[#AA8B56] ${metalMania.className}`}>{title.toUpperCase()}</h1>
+      <button className="flex flex-col border-4 border-[#5C4033] p-4 rounded-lg bg-[#395144] shadow-lg z-10 w-60 h-80 gap-2 cursor-pointer hover:bg-[#4E6C50]/80">
+         <h1 className={`text-2xl text-[#AA8B56] ${pirataOne.className}`}>{title.toUpperCase()}</h1>
          <img
-            className="w-full h-30 object-cover rounded"
+            className="w-full h-30 object-cover rounded border-2 p-1 border-[#AA8B56]"
             src={backgroundImageUrl}
             alt="Background Preview"
          />
-         <p>{description}</p>
-         <p>Game: {game}</p>
+         <p className="text-start">{description}</p>
+         <p className="text-start">Game: {game}</p>
       </button>
    )
 }
